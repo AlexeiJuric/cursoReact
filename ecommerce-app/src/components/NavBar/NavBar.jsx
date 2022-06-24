69cfa56a17c58156688663705e47149d07675eb7
AlexeiJuric <alexei@ufasta.edu.ar>
2022-06-23 21:52:31 -0300

    segunda entrega

--- ecommerce-app/src/components/NavBar/NavBar.jsx
+++ ecommerce-app/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,40 @@
-
+import { useState } from 'react';
+import './navbar.css'
 function NavBar() {
+  const [ menu , setMenu ] = useState( false )
+
+  const toggleMenu = () => {
+      setMenu( !menu )
+  }
+
   return (
-    <div>NavBar</div>
-  )
+  <header className="Cabecera">
+      <h1 className="Cabecera-h1">
+          <a href="#" className="Cabecera-a">
+              Registrate
+          </a>
+      </h1>
+
+      <button 
+          onClick={ toggleMenu }
+       className="Cabecera-button">
+      <svg className='Cabecera-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+      </button>
+
+      <nav className={ `Cabecera-nav ${ menu ? 'isActive' : '' }` }>
+          <ul className="Cabecera-ul">
+              <li className="Cabecera-li"><a href="#" className="Cabecera-a">Iniciar sesion</a></li>
+              <li className="Cabecera-li"><a href="#" className="Cabecera-a">Ver carrito</a></li>
+              <li className="Cabecera-li"><a href="#" className="Cabecera-a">Hombre</a></li>
+              <li className="Cabecera-li"><a href="#" className="Cabecera-a">Mujer</a></li>
+              <li className="Cabecera-li"><a href="#" className="Cabecera-a">Mas vendidos</a></li>
+          </ul>
+      </nav>
+
+  </header>
+)
 }
-export default NavBar;
+
+export default NavBar
