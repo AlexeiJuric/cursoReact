11fa9f1891a4b3e99feebab0eb4e744464fec2c8
AlexeiJuric <alexei@ufasta.edu.ar>
2022-07-02 14:49:19 -0300

    entrega 3

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import NavBar from 'components/NavBar'
-import ComponenteContenedor from './container/ComponenteContenedor';
+import NavBar from './components/NavBar/NavBar'
+import ComponenteContenedor from './components/container/ComponenteContenedor';
 function App() {
   return (
     <div className="App">
